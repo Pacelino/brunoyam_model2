ce9de6379fa532821bd89f3551f05df1796d688c
Pacelino <apple.pacel@gmail.com>
2021-08-09 14:40:19 +0300

    delet file on branch main

--- module_1/part_01/nt2.1.py
+++
@@ -1,9 +0,0 @@
-x = int(input('введите свое число '))
-y = int(input('введите свое число '))
-z = int(input('введите свое число '))
-if x == y and x == z and y == z:
-    print(3)
-elif x != y and x != z and y != z:
-    print(0)
-else:
-    print(2)
