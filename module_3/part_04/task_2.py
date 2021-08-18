2798b5db1e0ebf02acc7eaa2c34dee83f9f60e03
danilkushnerov <apple.pacel@gmail.com>
2021-08-18 20:43:47 +0300

    add file task_2

---
+++ module_3/part_04/task_2.py
@@ -0,0 +1,21 @@
+import json
+
+data_acc = dict()
+
+def register(login, password):
+    with open('data_acc.json', 'r') as f:
+       data_acc = json.load(f)
+       if login in data_acc.keys():
+           print('Логин занят')
+       if login not in data_acc.keys():
+           data_acc[login] = password
+           with open('data_acc.json', 'w') as f:
+               json.dump(data_acc, f)
+       else:
+           print('already exist')
+
+
+
+
+
+register(input("login"), input('password'))
