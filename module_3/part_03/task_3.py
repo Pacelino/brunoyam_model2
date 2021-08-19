416269ab8449dac628ba85e3c2fb9e5baab63761
danilkushnerov <apple.pacel@gmail.com>
2021-08-19 23:26:49 +0300

    kkk

--- module_3/part_03/task_3.py
+++ module_3/part_03/task_3.py
@@ -2,14 +2,32 @@ num = [56, 9, 11, 2]
 sort_num = sorted(num, reverse=True)
 num1 = []
 
-'''def summa(a):
-    result = 0
-    while a > 0:
-        result += a % 10
-        a //= 10'''
-for i in sort_num:
-    num1.append(str(i))
-print(num1, 'я не знаю как это делать, пусть бог мне может')
+def f_key_sort(x):
+    xf = float(x)
+
+    while (xf // 10) > 0:
+        xf /= 10
+
+    return xf
+
+
+def num_max(l):
+    l.sort(key=f_key_sort, reverse=True)
+
+    rs = ''
+
+    for elem in l:
+        rs += str(elem)
+
+    return int(rs)
+
+
+print('Введите список чисел через пробел: ')
+
+input_list = [int(x) for x in input().split()]
+
+print(num_max(input_list))
+
 
 
 
