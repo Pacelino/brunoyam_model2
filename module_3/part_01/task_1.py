57f3bf8b7ea835777eb16c5c0cfc699c18f5f423
danilkushnerov <apple.pacel@gmail.com>
2021-08-19 21:31:07 +0300

    переделал задание task_1 из папки part_01

--- module_3/part_01/task_1.py
+++ module_3/part_01/task_1.py
@@ -1,13 +1,11 @@
 x = int(input('введите свой вклад в банке: '))
-p = float(input('введите свой процент по вкладу: '))
+p = int(input('введите свой процент по вкладу: '))
 y = int(input('сколько хотите получить '))
-years_earnings = 0
+
 years_count = 0
-
-
-while x > y:
-    years_percent = x * p
-    x = x + years_earnings
+while x < y:
+    x *= 1 + p / 100
+    print(x)
+    x = int(100 * x) / 100
     years_count += 1
-
 print(years_count, 'лет до заветной суммы')
