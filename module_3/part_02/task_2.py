ea50e3f49938b3ac0e036e7449623d54fe0cc5b4
Daniil Kushnerov <87129167+Pacelino@users.noreply.github.com>
2021-08-19 22:14:27 +0300

    Update task_2.py

--- module_3/part_02/task_2.py
+++ module_3/part_02/task_2.py
@@ -5,8 +5,8 @@ n = 5
 m = [[randint(0, 100) for i in range(n)] for j in range(n)]
 mx_number = 0
 
-for i in m:
-    for j in i:
+for row m:
+    for element in i:
         if j > mx_number:
             mx_number = j
 print(mx_number)
