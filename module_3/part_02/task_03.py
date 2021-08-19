95e99a6afa1cbcb2e9e22ca8db07bd725aed6455
danilkushnerov <apple.pacel@gmail.com>
2021-08-19 22:34:27 +0300

    перbla bla bla

--- module_3/part_02/task_03.py
+++ module_3/part_02/task_03.py
@@ -1,8 +1,10 @@
 d = {'name1': 'id1', 'name2': 'id2', 'name3': 'id3'}
 res = dict()
-x = ''
-y = ''
-for i in d:
-    for j in d.values():
-        res[j] = i
-print(res)
+x = []
+
+for i in d.keys():
+    x.append(i)
+for j in d.values():
+
+    res[j] = x
+print(res, x)
