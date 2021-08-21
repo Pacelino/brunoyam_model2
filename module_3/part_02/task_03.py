1c66072d18c3e0a0e756ab6c657557723e8f206e
Daniil Kushnerov <87129167+Pacelino@users.noreply.github.com>
2021-08-21 18:35:52 +0300

    Update task_03.py
    переделал задание 3 часть 2

--- module_3/part_02/task_03.py
+++ module_3/part_02/task_03.py
@@ -1,10 +1,14 @@
-d = {'name1': 'id1', 'name2': 'id2', 'name3': 'id3'}
-res = dict()
-x = []
+d = {'name1': 'id', 'name2': 'id', 'name3': 'id'}
+revers = dict()
 
-for i in d.keys():
-    x.append(i)
-for j in d.values():
+for key, values in d.items():
+    if revers.get(values):
+        list = []
+        if type(revers[values]) == list:
+            list.extend(revers[values])
+            list.append(key)
+        else:
+            list.append(revers[values])
+            list.append(key)
 
-    res[j] = x
-print(res, x)
+        revers[values] = list
