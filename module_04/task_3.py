305fecfa036afa67043b49de511b623eda8b9089
danilkushnerov <apple.pacel@gmail.com>
2021-09-13 21:02:44 +0300

    module_04 task 3 fix1.0

--- module_04/task_3.py
+++ module_04/task_3.py
@@ -4,30 +4,23 @@ graph = {'1': ['2', '3'],
          '2': ['4', '5'],
          '3': ['1'],
          '4': ['2'],
-         '5': ['2']}
+         '5': ['2'],
+         '6': ['3']}
 
-def bfs(start, goal, graph):
-    queue = deque([start])
-    visited = {start: None}
-
-    while queue:
-        cur_node = queue.popleft()
-        if cur_node == goal:
-            break
-
-        next_nodes = graph[cur_node]
-        for next_node in next_nodes:
-            if next_node not in visited:
-                queue.append(next_node)
-                visited[next_node] = cur_node
-    return visited
-
-start = '1'
-goal = '5'
-visited = bfs(start, goal, graph)
-
-cur_node = goal
-print(f'\npath from {goal} to {start}: \n {goal} ', end='')
-while cur_node != start:
-    cur_node = visited[cur_node]
-    print(f'---> {cur_node} ', end='')
+def bfs(graph, start, search):
+    searchQueue = deque()  # очередь
+    searchQueue += graph[start]  # добавляем всех соседей начальной точки в очередь
+    visited = []  # посещенные вершины
+    while searchQueue:
+        node = searchQueue.popleft()  # вытащели первое число
+        if node not in visited:
+            if node == search:
+                print('Found')
+                print(visited)
+                return True
+            else:
+                searchQueue += graph[node] # добавляем всех соседей посещенной вершины
+                visited += [node]
+    print('not found')
+    return False
+bfs(graph, input('Начальная точка '), input('Искомая точка '))
