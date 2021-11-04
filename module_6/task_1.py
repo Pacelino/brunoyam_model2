196abc497664744186c1a6c9cbcc7a7ca73e8154
danilkushnerov <apple.pacel@gmail.com>
2021-11-05 01:12:46 +0300

    module_6 task_3

--- module_6/task_1.py
+++ module_6/task_1.py
@@ -5,9 +5,20 @@ name = ['Biba', "Boba", 'Alice']
 
 def get_thread(thread_name):
   time.sleep(1)
+  print(f'Thread started is name: {thread_name._name}')
 
-  print(f'Thread started is name: {thread_name}')
+
+thread = Thread(target=get_thread, args= (1,))
+thread2 = Thread(target=get_thread, args=thread)
+thread3 = Thread(target=get_thread)
+
+thread3.start()
+thread2.start()
+thread.start()
+
+thread3.join()
+thread2.join()
+thread.join()
 
 
-thread = [Thread(target=get_thread, args=i) for i in name ]
-get_thread(thread)
+get_thread(thread3)
--- module_6/task_3.py
+++ module_6/task_3.py
@@ -1,13 +1,36 @@
 import time
+import requests
 from threading import Thread
-name = ['Biba', "Boba", 'Alice']
+more_link = ['https://httpbin.org/get'], ['https://www.youtube.com/'], ['https://vk.com'], ['https://www.python.org/'], ['https://www.youtube.com/watch?v=xRQeXNZX_cQ']
 
 
-def get_thread(thread_name):
-  time.sleep(1)
+def get_html(link):
+  respone = requests.get(link)
+  return print(respone.text)
 
-  print(f'Thread started is name: {thread_name}')
+
+thread1 = Thread(target=get_html, args=more_link[0])
+thread2 = Thread(target=get_html, args=more_link[1])
+thread3 = Thread(target=get_html, args=more_link[2])
+thread4 = Thread(target=get_html, args=more_link[3])
+thread5 = Thread(target=get_html, args=more_link[4])
 
 
-thread = [Thread(target=get_thread, args=i) for i in name ]
-get_thread(thread)
+t1 = time.time()
+thread1.start()
+thread2.start()
+thread3.start()
+thread4.start()
+thread5.start()
+
+thread1.join()
+thread2.join()
+thread3.join()
+thread4.join()
+thread5.join()
+
+
+print(time.time()-t1)
+t2 = time.time()
+get_html('https://httpbin.org/get')
+print(time.time()-t2)
