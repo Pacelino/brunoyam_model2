4bc79e5b374e323ab357991e6d8d07d9778f02a7
danilkushnerov <apple.pacel@gmail.com>
2021-11-06 16:12:48 +0300

    module_6 task_1 fix (no comment)

--- module_6/task_1.py
+++ module_6/task_1.py
@@ -5,20 +5,18 @@ name = ['Biba', "Boba", 'Alice']
 
 def get_thread(thread_name):
   time.sleep(1)
-  print(f'Thread started is name: {thread_name._name}')
+  print(f'Thread started is name: {thread_name._name}\n')
 
+test = Thread()
+thread1 = Thread(target=get_thread, args=(test,))
+thread2 = Thread(target=get_thread, args=(thread1,))
+thread3 = Thread(target=get_thread, args=(thread2,))
+thread4 = Thread(target=get_thread, args=(thread3,))
+thread5 = Thread(target=get_thread, args=(thread4,))
 
-thread = Thread(target=get_thread, args= (1,))
-thread2 = Thread(target=get_thread, args=thread)
-thread3 = Thread(target=get_thread)
+more_thread = [thread1, thread2, thread3, thread4, thread5]
 
-thread3.start()
-thread2.start()
-thread.start()
-
-thread3.join()
-thread2.join()
-thread.join()
-
-
-get_thread(thread3)
+for t in more_thread:
+  t.start()
+for t in more_thread:
+  t.join()
