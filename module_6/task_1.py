3667b2fa158a2155062def492e0ef386ac332700
danilkushnerov <apple.pacel@gmail.com>
2021-11-06 16:08:38 +0300

    module_6 task_1 fix (no comment)

--- module_6/task_1.py
+++ module_6/task_1.py
@@ -15,20 +15,10 @@ thread4 = Thread(target=get_thread, args=(thread3,))
 thread5 = Thread(target=get_thread, args=(thread4,))
 
 more_thread = [thread1, thread2, thread3, thread4, thread5]
-# thread1.start()
-# thread2.start()
-# thread3.start()
-# thread4.start()
-# thread5.start()
+
 for t in more_thread:
   t.start()
 for t in more_thread:
   t.join()
-# thread1.join()
-# thread2.join()
-# thread3.join()
-# thread4.join()
-# thread5.join()
 
 
-# get_thread(thread3)
