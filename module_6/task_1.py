6f60084135555fe09eff3600664a919fb7a8e303
danilkushnerov <apple.pacel@gmail.com>
2021-11-06 16:06:13 +0300

    module_6 task_1 fix

--- module_6/task_1.py
+++ module_6/task_1.py
@@ -1,24 +1,34 @@
 import time
 from threading import Thread
-name = ['Biba', "Boba", 'Alice']
 
 
 def get_thread(thread_name):
   time.sleep(1)
-  print(f'Thread started is name: {thread_name._name}')
+  print(f'Thread started is name: {thread_name._name}\n')
+
+test = Thread()
+
+thread1 = Thread(target=get_thread, args=(test,))
+thread2 = Thread(target=get_thread, args=(thread1,))
+thread3 = Thread(target=get_thread, args=(thread2,))
+thread4 = Thread(target=get_thread, args=(thread3,))
+thread5 = Thread(target=get_thread, args=(thread4,))
+
+more_thread = [thread1, thread2, thread3, thread4, thread5]
+# thread1.start()
+# thread2.start()
+# thread3.start()
+# thread4.start()
+# thread5.start()
+for t in more_thread:
+  t.start()
+for t in more_thread:
+  t.join()
+# thread1.join()
+# thread2.join()
+# thread3.join()
+# thread4.join()
+# thread5.join()
 
 
-thread = Thread(target=get_thread, args= (1,))
-thread2 = Thread(target=get_thread, args=thread)
-thread3 = Thread(target=get_thread)
-
-thread3.start()
-thread2.start()
-thread.start()
-
-thread3.join()
-thread2.join()
-thread.join()
-
-
-get_thread(thread3)
+# get_thread(thread3)
